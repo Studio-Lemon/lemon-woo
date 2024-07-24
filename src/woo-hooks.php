b209ce08fa79aad9c2d785362f8377b22d66cfb2
Erik van der Bas <erik@basedonline.nl>
2024-07-24 11:53:58 +0200

    fixes lemon woo

--- src/woo-hooks.php
+++ src/woo-hooks.php
@@ -2,10 +2,17 @@
 
 namespace WP_Lemon\Plugin\Lemon_Woo;
 
-function product_zoom_options( $zoom_options ) {
+function product_zoom_options($zoom_options)
+{
 	// Changing the magnification level:
 	$zoom_options['magnify'] = 0.7;
 
 	return $zoom_options;
 }
-add_filter( 'woocommerce_single_product_zoom_options', __NAMESPACE__ . '\product_zoom_options' );
+add_filter('woocommerce_single_product_zoom_options', __NAMESPACE__ . '\product_zoom_options');
+
+
+add_action('init', function () {
+	remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
+	add_action('wp-lemon/action/entry/content/after', 'woocommerce_output_related_products');
+});
