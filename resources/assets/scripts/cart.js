408f2c3c8d1a6c8b33ab4681728eb0b887b9e56f
Erik van der Bas <erik@basedonline.nl>
2025-10-07 16:54:45 +0200

    remove console log

--- resources/assets/scripts/cart.js
+++ resources/assets/scripts/cart.js
@@ -1,5 +1,4 @@
 export default function () {
-   console.log('cart.js');
    document.body.addEventListener('added_to_cart', function () {
       var count = document.querySelector('.js-cart-count').textContent;
       document.getElementById('js-cart-link').dataset.count = count;
