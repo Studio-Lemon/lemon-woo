d4563de68ab56aaea028a1e699d9ea0b1f0b87df
Erik van der Bas <erik@basedonline.nl>
2025-11-18 15:48:12 +0100

    new cart version

--- resources/assets/scripts/cart.js
+++ resources/assets/scripts/cart.js
@@ -1,7 +1,7 @@
 export default function () {
    document.body.addEventListener('added_to_cart', function () {
       var count = document.querySelector('.js-cart-count').textContent;
-      document.getElementById('js-cart-link').dataset.count = count;
+      document.querySelector('.js-cart-link').dataset.count = count;
    });
 
 }
